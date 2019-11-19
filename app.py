4b05089c235b355c6b822599a9caa2dfba277f2a
Michael Lin <mlin01@stuy.edu>
2019-11-19 08:33:27 -0500

    organizing folders

--- app.py
+++ app.py
@@ -1,14 +1,4 @@
-import os
-
-from flask import Flask, render_template, request, flash, redirect, session, abort
-
-app = Flask(__name__)
-
-app.secret_key = os.urandom(64)
-
-@app.route('/')
-def home():
-    return render_template("base.html")
+from app import app
 
 if __name__ == '__main__':
     app.run(debug=True)
