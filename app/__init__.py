dfab03262105403d3c50b5364a64f428ade9a3ad
Michael Lin <michaellin01@gmail.com>
2019-11-24 22:38:39 -0500

    working on food data searching

--- app/__init__.py
+++ app/__init__.py
@@ -24,11 +24,27 @@ def recipe():
 def recipeSearch():
     data = None
     if (request.form):
-        call = "http://www.recipepuppy.com/api/?q={}&p=1".format(request.form['query'])
-        url = urllib.request.urlopen( call)
-        response = url.read()
+        url = "http://www.recipepuppy.com/api/?q={}&p=1".format(request.form['query'])
+        req = urllib.request.urlopen(url)
+        response = req.read()
         data = json.loads(response)['results']
     return render_template( 'recipe_search.html', title = "Recipe Search", data = data)
+
+@app.route( '/fooddata', methods=['GET', 'POST'])
+def fooddata():
+    data = None
+    if (request.form):
+        query = request.form['query']
+        # headers = {
+        #     "generalSearchInput": query
+        # }
+        url = "https://api.nal.usda.gov/fdc/v1/340946?api_key=eVfCzyFo4P5Aoie9Lt1kniHK7iUfafWXNMYYbwsl"
+        req = urllib.request.Request(url)
+        data = json.loads(urllib.request.urlopen(req).read())
+        print(data)
+        #response = call.read()
+        #data = json.loads(response)['results']
+    return render_template('food_data.html', title = 'Food Data', data = data)
 
 @app.route('/restaurant')
 def restaurant():
