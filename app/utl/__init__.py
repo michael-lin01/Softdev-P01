3a3b3ce3c0ce3c68093321233fe44644dbe6a185
Michael Lin <michaellin01@gmail.com>
2019-12-02 18:57:52 -0500

    working entries page

--- app/utl/__init__.py
+++ app/utl/__init__.py
@@ -27,8 +27,8 @@ def build_db():
     command = "CREATE TABLE IF NOT EXISTS user (id INTEGER PRIMARY KEY, username TEXT, password BLOB)"
     execute(command)    # run SQL statement
 
-    command = "CREATE TABLE IF NOT EXISTS blog (user_id INT, breakfast TEXT, lunch TEXT, dinner TEXT, snacks TEXT, restaurant TEXT, entry_date TEXT)"
+    command = "CREATE TABLE IF NOT EXISTS blog (user_id INT, breakfast TEXT, lunch TEXT, dinner TEXT, snacks TEXT, restaurant TEXT, entry_date DATE)"
     execute(command)    # run SQL statement
-
+    
     command = "CREATE TABLE IF NOT EXISTS cache (query INT, response TEXT)"
     execute(command)    # run SQL statement
--- app/utl/blog.py
+++ app/utl/blog.py
@@ -4,7 +4,7 @@ import sqlite3
 
 class Blog:
     def __init__(self, id):
-        command = 'SELECT * FROM blog WHERE user_id={}'.format(id)
+        command = 'SELECT * FROM blog WHERE user_id={} ORDER BY entry_date'.format(id)
         data = execute(command).fetchall()
         self.id = id
         self.data = data
@@ -13,5 +13,11 @@ class Blog:
     @staticmethod
     def add_entry(user_id, breakfast, lunch, dinner, snacks, restaurant, entry_date):
         command = 'INSERT INTO blog (user_id, breakfast, lunch, dinner, snacks, restaurant, entry_date) \
-                    VALUES ({}, "{}", "{}", "{}", "{}", "{}", "{}")'.format(user_id, breakfast, lunch, dinner, snacks, restaurant, entry_date)
+            VALUES ({}, "{}", "{}", "{}", "{}", "{}", "{}")'.format(user_id, breakfast, lunch, dinner, snacks, restaurant, entry_date)
         execute(command)
+    
+    @staticmethod
+    def check_date_taken(user_id, entry_date):
+        command = 'SELECT * FROM blog WHERE user_id=%s AND entry_date="%s"' % (user_id, entry_date)
+        data = execute(command).fetchall()
+        return len(data) > 0
